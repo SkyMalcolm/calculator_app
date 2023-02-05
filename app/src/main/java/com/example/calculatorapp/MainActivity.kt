3b5609455b7fb8d320d432766a20850e58125fcb
SkyMalcolm <malcolm.lennartsson@iths.se>
2023-02-05 17:32:29 +0100

    UI and working inputfield

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,36 +3,83 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+               Surface(
+                   modifier = Modifier.fillMaxSize(),
+                   color = MaterialTheme.colors.background
+               ) {
+                   CalculatorScreen()
+               }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CalculatorScreen() {
+    Column(modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        InputNumberField()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun InputNumberField() {
+    var amountInput by remember { mutableStateOf("") }
+    TextField(
+        value = amountInput,
+        onValueChange = {amountInput = it},
+        label = {
+            Text(
+                text = stringResource(id = R.string.bill_amount),
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CalculatorAppTheme {
-        Greeting("Android")
+        CalculatorScreen()
     }
 }
+
